eb1ea101ae5cbd8a80bc5542a19e0f1867845226
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-09-17 18:39:25 +0200

    Create editing-mix.js

---
+++ editing-mix.js
@@ -0,0 +1,205 @@
+var obj = JSON.parse($response.body);
+
+obj={
+  "metronome_pro" : {
+    "android" : {
+      "metronome_pro.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "metronome_pro.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "metronome_pro.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "metronome_pro.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "equalizer_plus" : {
+    "android" : {
+      "equalizer_plus.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "equalizer_plus.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "equalizer_plus.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "equalizer_plus.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "learning_drums" : {
+    "android" : {
+      "learning_drums.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_drums.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "learning_drums.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_drums.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "learning_sampler" : {
+    "android" : {
+      "learning_sampler.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_sampler.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "learning_sampler.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_sampler.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "bassbooster" : {
+    "ios" : {
+      "bassbooster.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "bassbooster.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "edjing_mix" : {
+    "android" : {
+      "edjing_mix.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "edjing_mix.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "edjing_mix.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "edjing_mix.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "drummy_machine" : {
+    "android" : {
+      "drummy_machine.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "drummy_machine.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_activeTrue" : true
+      }
+    },
+    "ios" : {
+      "drummy_machine.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "drummy_machine.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "tuner_guitar" : {
+    "android" : {
+      "tuner_guitar.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "tuner_guitar.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "tuner_guitar.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "tuner_guitar.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "edjing_pro" : {
+    "android" : {
+      "edjing_pro.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "edjing_pro.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "edjing_pro.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "edjing_pro.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "learning_piano" : {
+    "android" : {
+      "learning_piano.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_piano.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "learning_piano.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_piano.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "learning_guitar" : {
+    "android" : {
+      "learning_guitar.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_guitar.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    },
+    "ios" : {
+      "learning_guitar.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "learning_guitar.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  },
+  "beatwave" : {
+    "ios" : {
+      "beatwave.features.premium" : {
+        "expire_at" : "2090-06-30T18:00:53Z",
+        "id" : "beatwave.features.premium",
+        "expire_at_ts" : 3802528853,
+        "is_active" : true
+      }
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});
